7e2f4d3e60a5ac435a11451e2089a0a067b557b5
gyangho <52252845+gyangho@users.noreply.github.com>
2025-07-17 23:10:55 +0900

    컴파일해줘

--- UCMS/UCMS.js
+++ UCMS/UCMS.js
@@ -123,7 +123,7 @@ function onMessage(msg) {}
 function onCommand(msg) {
   const content = msg.content.slice(PREFIX.length);
 
-  if (content === "컴파일해줘") {
+  if (content === "컴파일해줘" || content === " 컴파일해줘") {
     try {
       bot.compile();
     } catch (err) {
